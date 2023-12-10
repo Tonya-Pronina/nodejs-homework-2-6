36485de9d025e3f902184f7e9e06e7c3ff473b8c
Tonya-Pronina <darkpixus@gmail.com>
2023-12-11 01:53:55 +0200

    done

---
+++ models/index.js
@@ -0,0 +1,21 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("./contacts");
+
+// const { nanoid } = require("nanoid");
+
+// module.exports = {
+//   getNanoid: () => nanoid,
+// };
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
---
+++ schemas/contacts-schemas.js
@@ -0,0 +1,15 @@
+const Joi = require("joi");
+
+const schema = Joi.object({
+  name: Joi.string().required().messages({
+    "any.required": `"name" must be exist`,
+  }),
+  email: Joi.string().required().messages({
+    "any.required": `"email" must be exist`,
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": `"phone" must be exist`,
+  }),
+});
+
+module.exports = schema;
